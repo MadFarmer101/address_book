88372332e4370273206dd339dc29b25886706a69
Janko <mailodjanka@gmail.com>
2020-01-28 13:40:27 +0100

    Created first cypress tests and modified index to pass the tests

---
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -0,0 +1,15 @@
+describe ('user can create a contact', () => {
+    it('tests if user is able to create a contact', () => {
+        cy.visit('http://localhost:3005')
+        cy.get('#add-contact').click()
+        cy.get('#name').type('Janko')
+        cy.get('#email').type('janko@email.com')
+        cy.get('#phone').type('0702222555')
+        cy.get('#twitter').type('@janko')
+        cy.get('#submit').click()
+    })
+
+    it('displays a name of a new contact', () => {
+        cy.get('#contact-list').should('contain', 'Janko')
+    })
+})
